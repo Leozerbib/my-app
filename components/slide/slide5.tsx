3db4d729c8394e834befd044ac0e201d1fddfb3c
Leozerbib <116356061+Leozerbib@users.noreply.github.com>
2024-12-02 21:50:09 +0100

    graph

--- components/slide/slide5.tsx
+++ components/slide/slide5.tsx
@@ -7,6 +7,7 @@ import ma  from '@/public/matteo.jpg';
 import li  from '@/public/lilian.jpg';
 import le  from '@/public/leo.jpg';
 import re  from '@/public/remi.jpg';
+import logo from '@/public/logo.jpg';
 
 const OpeningSlides = () => {
   return (
@@ -19,35 +20,35 @@ const OpeningSlides = () => {
           
           <div className="h-[80%] w-full gap-10 flex items-center justify-center mt-8">
             <div className="text-center">
-              <div className="w-48 h-48 mx-auto bg-gray-200 rounded-full mb-2">
+              <div className="w-60 h-60 mx-auto bg-gray-200 rounded-full mb-2">
                 <Image src={ju} alt="Julien" className="rounded-full" />
               </div>
               <h3 className="font-semibold">Julien Tournier</h3>
               <p className="text-lg">Responsable Technique</p>
             </div>
             <div className="text-center">
-              <div className="w-48 h-48 mx-auto bg-gray-200 rounded-full mb-2">
+              <div className="w-60 h-60 mx-auto bg-gray-200 rounded-full mb-2">
               <Image src={ma} alt="Julien" className="rounded-full" />
               </div>
               <h3 className="font-semibold">Matteo Cruz</h3>
               <p className="text-lg">Responsable Marketing</p>
             </div>
             <div className="text-center">
-              <div className="w-48 h-48 mx-auto bg-gray-200 rounded-full mb-2">
+              <div className="w-60 h-60 mx-auto bg-gray-200 rounded-full mb-2">
               <Image src={li} alt="Julien" className="rounded-full" />
               </div>
               <h3 className="font-semibold">Lilian Mayet</h3>
               <p className="text-lg">Responsable Financier</p>
             </div>
             <div className="text-center">
-              <div className="w-48 h-48 mx-auto bg-gray-200 rounded-full mb-2">
+              <div className="w-60 h-60 mx-auto bg-gray-200 rounded-full mb-2">
               <Image src={le} alt="Julien" className="rounded-full" />
               </div>
               <h3 className="font-semibold">Léo Zerbib</h3>
               <p className="text-lg">Responsable Produit</p>
             </div>
             <div className="text-center">
-              <div className="w-48 h-48 mx-auto bg-gray-200 rounded-full mb-2">
+              <div className="w-60 h-60 mx-auto bg-gray-200 rounded-full mb-2">
               <Image src={re} alt="Julien" className="rounded-full" />
               </div>
               <h3 className="font-semibold">Rémi Godet</h3>
@@ -61,45 +62,50 @@ const OpeningSlides = () => {
       <Card className="p-6 bg-white shadow-lg">
         <div className="space-y-4 h-full w-full">
           <h2 className="text-6xl font-bold text-blue-800">Pourquoi des Lunettes Intelligentes ?</h2>
-          
-          <div className="h-[80%] w-full relative">
-            <div className="absolute w-1 bg-blue-200 h-full left-4"></div>
-            <div className="space-y-6 ml-12 flex h-full flex-col w-full">
-              <div className="relative h-1/3 w-full">
-                <div className="absolute  w-16 h-16 bg-blue-500 rounded-full flex items-center justify-center">
-                  <Clock className="w-12 h-12 text-white" />
+          <div className='h-[80%] w-full flex'>
+            <div className="h-full w-1/2">
+                <div className="absolute w-1 bg-blue-200 h-[80%] left-4"></div>
+                <div className="space-y-6 ml-12 flex h-full flex-col w-full">
+                <div className="relative h-1/3 w-full">
+                    <div className="absolute  w-16 h-16 bg-blue-500 rounded-full flex items-center justify-center">
+                    <Clock className="w-12 h-12 text-white" />
+                    </div>
+                    <div className='flex-col flex text-3xl pt-8 justify-center w-full h-full'>
+                    <h3 className="font-semibold">1950-1980</h3>
+                    <p>Premiers appareils auditifs électroniques</p>
+                    <p>Débuts de l'assistance technologique aux seniors</p>
+                    </div>
                 </div>
-                <div className='flex-col flex text-3xl pt-8 justify-center w-full h-full'>
-                  <h3 className="font-semibold">1950-1980</h3>
-                  <p>Premiers appareils auditifs électroniques</p>
-                  <p>Débuts de l'assistance technologique aux seniors</p>
+                
+                <div className="relative h-1/3 w-full">
+                    <div className="absolute  w-16 h-16 bg-blue-500 rounded-full flex items-center justify-center">
+                    <Users className="w-12 h-12 text-white" />
+                    </div>
+                    <div className='flex-col flex text-3xl pt-8 justify-center w-full h-full'>
+                    <h3 className="font-semibold">1980-2010</h3>
+                    <p>Émergence des dispositifs de télésurveillance</p>
+                    <p>Développement des systèmes d'alerte</p>
+                    </div>
                 </div>
-              </div>
-              
-              <div className="relative h-1/3 w-full">
-                <div className="absolute  w-16 h-16 bg-blue-500 rounded-full flex items-center justify-center">
-                  <Users className="w-12 h-12 text-white" />
+                
+                <div className="relative h-1/3 w-full">
+                    <div className="absolute  w-16 h-16 bg-blue-500 rounded-full flex items-center justify-center">
+                    <Brain className="w-12 h-12 text-white" />
+                    </div>
+                    <div className='flex-col flex text-3xl pt-8 justify-center w-full h-full'>
+                    <h3 className="font-semibold">2010-2023</h3>
+                    <p>Révolution des objets connectés</p>
+                    <p>Intelligence artificielle et réalité augmentée</p>
+                    </div>
                 </div>
-                <div className='flex-col flex text-3xl pt-8 justify-center w-full h-full'>
-                  <h3 className="font-semibold">1980-2010</h3>
-                  <p>Émergence des dispositifs de télésurveillance</p>
-                  <p>Développement des systèmes d'alerte</p>
                 </div>
-              </div>
-              
-              <div className="relative h-1/3 w-full">
-                <div className="absolute  w-16 h-16 bg-blue-500 rounded-full flex items-center justify-center">
-                  <Brain className="w-12 h-12 text-white" />
-                </div>
-                <div className='flex-col flex text-3xl pt-8 justify-center w-full h-full'>
-                  <h3 className="font-semibold">2010-2023</h3>
-                  <p>Révolution des objets connectés</p>
-                  <p>Intelligence artificielle et réalité augmentée</p>
-                </div>
-              </div>
             </div>
+            <div className="h-full flex justify-center items-center w-1/2 relative">
+                <Image src={logo} alt="Julien" className="rounded-full" />
+            </div>
+            </div>
+            
           </div>
-        </div>
       </Card>
 
       {/* Slide 3 */}
